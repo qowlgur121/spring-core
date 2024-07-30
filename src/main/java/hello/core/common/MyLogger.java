ab9735ae4e56c9659403791d0fcb497cf7032f94
Jihyeok <qowlgur121@gmail.com>
2024-07-30 18:00:14 +0900

    프록시로 리팩토링

--- src/main/java/hello/core/common/MyLogger.java
+++ src/main/java/hello/core/common/MyLogger.java
@@ -3,34 +3,35 @@ package hello.core.common;
 import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
 import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.stereotype.Component;
 
 import java.util.UUID;
 
 @Component
-@Scope("request")
+@Scope(value = "request", proxyMode = ScopedProxyMode.TARGET_CLASS)
 public class MyLogger {
-    //로그를 출력하기 위한 클래스
+    //스프링의 request스코프를 사용하여 각 웹 요청마다 고유한 로그 메시지를 출력하기 위한 클래스
 
-    private String uuid;
-    private String requestURL;
+    private String uuid; //Mylogger 객체가 생성될 때마다 생성되는 유니크 아이디
+    private String requestURL; //요청된 웹 요청의 주소
 
-    public void setRequestURL(String requestURL) {
+    public void setRequestURL(String requestURL) { //MyLogger 인스턴스와 특정 웹 요청과 연결
         this.requestURL = requestURL;
     }
 
-    public void log(String message) {
+    public void log(String message) { //콘솔에 로그 출력
         System.out.println("[" + uuid + "]" + "[" + requestURL + "]" + message);
     }
 
     @PostConstruct
-    public void init() {
-        uuid = UUID.randomUUID().toString(); //유니크아이디 생성
+    public void init() { //스프링 빈 생성 후 바로 유니크 아이디 생성
+        uuid = UUID.randomUUID().toString();
         System.out.println("[" + uuid + "] request scope bean create:" + this);
     }
 
     @PreDestroy
-    public void destroy() {
+    public void destroy() { //빈 소멸 직전에 소멸 메시지 출력
         System.out.println("[" + uuid + "] request scope bean close:" + this);
     }
 }
--- src/main/java/hello/core/web/LogDemoController.java
+++ src/main/java/hello/core/web/LogDemoController.java
@@ -1,8 +1,10 @@
 package hello.core.web;
 
 import hello.core.common.MyLogger;
+import hello.core.logdemo.LogDemoService;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -14,14 +16,16 @@ public class LogDemoController {
     private final LogDemoService logDemoService;
     private final MyLogger myLogger;
 
-    @RequestMapping("log-demo")
-    @ResponseBody
+    @RequestMapping("log-demo") //"/log-domo" URL에 대한 요청을 처리하도록 매핑
+    @ResponseBody //반환값을 예쁘게(HTML, JSP 등) 포장하지 않고 바로 택배상자에 넣기(JSON, XML 등)
     public String logDemo(HttpServletRequest request) {
-        String requestURL = request.getRequestURI().toString();
-        myLogger.setRequestURL(requestURL);
+        String requestURL = request.getRequestURI().toString(); //요청 URL을 추출한다. ex)http://localhost:8080/log-demo
 
-        myLogger.log("controller test");
+        System.out.println("myLogger = " + myLogger.getClass());
+        myLogger.setRequestURL(requestURL); //추출한 URL을 현재 요청에 할당된 MyLogger 인스턴스에 저장한다.
+
+        myLogger.log("controller test"); //로그에 메시지를 남김
         logDemoService.logic("testId");
-        return "OK";
+        return "OK"; //HTTP 응답으로 "OK" 문자열 반환
     }
 }
